345d96b2c2b23f29834e833c8146328f9873a029
emagidson <emagidson@wustl.edu>
2016-10-03 08:48:00 -0500

    Add files via upload

---
+++ js4.js
@@ -0,0 +1,435 @@
+$(document).ready(function() {
+
+// var ad = Math.round(Math.random() * 255);
+//            var bd = Math.round(Math.random() * 255);
+//            var cd = Math.round(Math.random() * 255);
+//            $( ".top" ).css("background-color", "RGB(" + ad + "," + bd + "," + cd + ")");
+//            $( ".bottom" ).css("background-color", "RGB(" + ad + "," + bd + "," + cd + ")");
+	
+
+var color = ["#FF5F00", "orange", "yellow", "blue", "#00F3FF", "#8BFF00", "#AA00E8", "white"];
+var random = Math.round(Math.random() * 7);
+ $( ".top" ).css("background-color", color[random]);
+$( ".bottom" ).css("background-color", color[random]);
+
+// alert(random);
+
+
+	// $('#tri1').hover('click touchstart', function() {
+	// 	$('.top').animate({borderTop: '40px'}, 600);
+	// });
+
+
+	$('#tri2').hover('click touchstart', function() {
+		var color = ["#FF5F00", "orange", "yellow", "blue", "#00F3FF", "#8BFF00", "#AA00E8", "white"];
+var random2 = Math.round(Math.random() * 7);
+ $( ".top" ).css("background-color", color[random2]);
+$( ".bottom" ).css("background-color", color[random2]);
+	
+		$('.bottom').animate({opacity: '.21'}, 10000);
+		$('.group1').animate({opacity: '.3'}, 2000);
+		$('.group2').animate({opacity: '.3'}, 2000);
+		$('.group3').animate({opacity: '.3'}, 2000);
+		$('.group4').animate({opacity: '.3'}, 2000);
+		$('.group5').animate({opacity: '.3'}, 2000);
+		$('.group6').animate({opacity: '.3'}, 2000);
+		$('.group7').animate({opacity: '.3'}, 2000);
+		$('.group8').animate({opacity: '.3'}, 2000);
+		$('.group9').animate({opacity: '.3'}, 2000);
+		$('.group10').animate({opacity: '.3'}, 2000);
+		$('.group11').animate({opacity: '.3'}, 2000);
+		$('.group12').animate({opacity: '.3'}, 2000);
+		$('.group13').animate({opacity: '.3'}, 2000);
+		$('.group14').animate({opacity: '.3'}, 2000);
+		$('.group15').animate({opacity: '.3'}, 2000);
+		$('.group16').animate({opacity: '.3'}, 2000);
+		$('.group17').animate({opacity: '.3'}, 2000);
+		$('.group18').animate({opacity: '.3'}, 2000);
+		$('.group19').animate({opacity: '.3'}, 2000);
+		$('.group20').animate({opacity: '.3'}, 2000);
+		$('.group21').animate({opacity: '.3'}, 2000);
+		$('.group22').animate({opacity: '.3'}, 2000);
+		$('.top').animate({opacity: '.22'}, 2000);
+		$('.bottom').animate({opacity: '.19'}, 2000);
+
+	});
+
+
+	$('#tri8').hover('click touchstart', function() {
+	var color = ["#FF5F00", "orange", "yellow", "blue", "#00F3FF", "#8BFF00", "#AA00E8", "white"];
+var random3 = Math.round(Math.random() * 7);
+ $( ".top" ).css("background-color", color[random3]);
+$( ".bottom" ).css("background-color", color[random3]);
+
+		$('.bottom').animate({opacity: '.21'}, 10000);
+		$('.group1').animate({left: '-=1000px'}, 2000);
+		$('.group2').animate({left: '-=1000px'}, 2000);
+		$('.group3').animate({left: '-=1000px'}, 2000);
+		$('.group4').animate({left: '+=1000px'}, 2000);
+		$('.group5').animate({left: '+=1000px'}, 2000);
+		$('.group6').animate({left: '-=1000px'}, 2000);
+		$('.group7').animate({left: '-=1000px'}, 2000);
+		$('.group8').animate({left: '-=1000px'}, 2000);
+		$('.group9').animate({left: '+=1000px'}, 2000);
+		$('.group10').animate({left: '+=1000px'}, 2000);
+		$('.group11').animate({left: '+=1000px'}, 2000);
+		$('.group12').animate({left: '-=1000px'}, 2000);
+		$('.group13').animate({left: '-=1000px'}, 2000);
+		$('.group14').animate({left: '+=1000px'}, 2000);
+		$('.group15').animate({left: '+=1000px'}, 2000);
+		$('.group16').animate({left: '+=1000px'}, 2000);
+		$('.group17').animate({left: '-=1000px'}, 2000);
+		$('.group18').animate({left: '-=1000px'}, 2000);
+		$('.group19').animate({left: '-=1000px'}, 2000);
+		$('.group20').animate({left: '+=1000px'}, 2000);
+		$('.group21').animate({left: '+=1000px'}, 2000);
+		$('.group22').animate({left: '+=1000px'}, 2000);
+
+		$('.group1').animate({left: '+=4000px'}, 4000);
+		$('.group2').animate({left: '+=4000px'}, 4000);
+		$('.group3').animate({left: '+=4000px'}, 4000);
+		$('.group4').animate({left: '-=4000px'}, 4000);
+		$('.group5').animate({left: '-=4000px'}, 4000);
+		$('.group6').animate({left: '+=4000px'}, 4000);
+		$('.group7').animate({left: '+=4000px'}, 4000);
+		$('.group8').animate({left: '+=4000px'}, 4000);
+		$('.group9').animate({left: '-=4000px'}, 4000);
+		$('.group10').animate({left: '-=4000px'}, 4000);
+		$('.group11').animate({left: '-=4000px'}, 4000);
+		$('.group12').animate({left: '+=4000px'}, 4000);
+		$('.group13').animate({left: '+=4000px'}, 4000);
+		$('.group14').animate({left: '-=4000px'}, 4000);
+		$('.group15').animate({left: '-=4000px'}, 4000);
+		$('.group16').animate({left: '-=4000px'}, 4000);
+		$('.group17').animate({left: '+=4000px'}, 4000);
+		$('.group18').animate({left: '+=4000px'}, 4000);
+		$('.group19').animate({left: '+=4000px'}, 4000);
+		$('.group20').animate({left: '-=4000px'}, 4000);
+		$('.group21').animate({left: '-=4000px'}, 4000);
+		$('.group22').animate({left: '-=4000px'}, 4000);
+
+		$('.group1').animate({left: '-=3000px'}, 2000);
+		$('.group2').animate({left: '-=3000px'}, 2000);
+		$('.group3').animate({left: '-=3000px'}, 2000);
+		$('.group4').animate({left: '+=3000px'}, 2000);
+		$('.group5').animate({left: '+=3000px'}, 2000);
+		$('.group6').animate({left: '-=3000px'}, 2000);
+		$('.group7').animate({left: '-=3000px'}, 2000);
+		$('.group8').animate({left: '-=3000px'}, 2000);
+		$('.group9').animate({left: '+=3000px'}, 2000);
+		$('.group10').animate({left: '+=3000px'}, 2000);
+		$('.group11').animate({left: '+=3000px'}, 2000);
+		$('.group12').animate({left: '-=3000px'}, 2000);
+		$('.group13').animate({left: '-=3000px'}, 2000);
+		$('.group14').animate({left: '+=3000px'}, 2000);
+		$('.group15').animate({left: '+=3000px'}, 2000);
+		$('.group16').animate({left: '+=3000px'}, 2000);
+		$('.group17').animate({left: '-=3000px'}, 2000);
+		$('.group18').animate({left: '-=3000px'}, 2000);
+		$('.group19').animate({left: '-=3000px'}, 2000);
+		$('.group20').animate({left: '+=3000px'}, 2000);
+		$('.group21').animate({left: '+=3000px'}, 2000);
+		$('.group22').animate({left: '+=3000px'}, 2000);
+
+	});
+
+
+	$('#tri4').hover('click touchstart', function() {
+		var color = ["#FF5F00", "orange", "yellow", "blue", "#00F3FF", "#8BFF00", "#AA00E8", "white"];
+var random4 = Math.round(Math.random() * 7);
+ $( ".top" ).css("background-color", color[random4]);
+$( ".bottom" ).css("background-color", color[random4]);
+		$('.top').animate({width: '3000'}, 3000);
+		$('.top').animate({width: '0'}, 2000);
+	});
+
+		$('#tri6').hover('click touchstart', function() {
+		var color = ["#FF5F00", "orange", "yellow", "blue", "#00F3FF", "#8BFF00", "#AA00E8", "white"];
+var random5 = Math.round(Math.random() * 7);
+ $( ".top" ).css("background-color", color[random5]);
+$( ".bottom" ).css("background-color", color[random5]);
+		$('.bottom').animate({width: '3000'}, 2000);
+		$('.bottom').animate({width: '0'}, 3000);
+	});
+
+	// 	$('#tri71').hover('click touchstart', function() {
+	// 	$('.top').animate({width: '3000', left: '-=3000'}, 4000);
+	// 	$('.top').animate({width: '0', left: '+=3000'}, 4000);
+	// });
+
+
+	$('#tri12').hover('click touchstart', function() {
+		var color = ["#FF5F00", "orange", "yellow", "blue", "#00F3FF", "#8BFF00", "#AA00E8", "white"];
+var random6 = Math.round(Math.random() * 7);
+ $( ".top" ).css("background-color", color[random6]);
+$( ".bottom" ).css("background-color", color[random6]);
+		$('.top').animate({left: '+=70px'}, 1000);
+		$('.top').animate({left: '-=70px'}, 1000);
+		$('.top').animate({left: '-=70px'}, 1000);
+		$('.top').animate({left: '+=70px'}, 1000);
+	});
+
+		$('#tri14').hover('click touchstart', function() {
+		var color = ["#FF5F00", "orange", "yellow", "blue", "#00F3FF", "#8BFF00", "#AA00E8", "white"];
+var random7 = Math.round(Math.random() * 7);
+ $( ".top" ).css("background-color", color[random7]);
+$( ".bottom" ).css("background-color", color[random7]);
+		$('.bottom').animate({height: '+=400'}, 4000);
+		$('.bottom').animate({height: '-=400'}, 4000);
+		
+	});
+
+		$('#tri10').hover('click touchstart', function() {
+			var color = ["#FF5F00", "orange", "yellow", "blue", "#00F3FF", "#8BFF00", "#AA00E8", "white"];
+var random8 = Math.round(Math.random() * 7);
+ $( ".top" ).css("background-color", color[random8]);
+$( ".bottom" ).css("background-color", color[random8]);
+		$('.group1').animate({height: '220'}, 2000);
+		$('.group2').animate({height: '220'}, 2000);
+		$('.group3').animate({height: '220'}, 2000);
+		$('.group4').animate({height: '220'}, 2000);
+		$('.group5').animate({height: '220'}, 2000);
+		$('.group6').animate({height: '220'}, 2000);
+		$('.group7').animate({height: '220'}, 2000);
+		$('.group8').animate({height: '220'}, 2000);
+		$('.group9').animate({height: '220'}, 2000);
+		$('.group10').animate({height: '220'}, 2000);
+		$('.group11').animate({height: '220'}, 2000);
+		$('.group12').animate({height: '220'}, 2000);
+		$('.group13').animate({height: '220'}, 2000);
+		$('.group14').animate({height: '220'}, 2000);
+		$('.group15').animate({height: '220'}, 2000);
+		$('.group16').animate({height: '220'}, 2000);
+		$('.group17').animate({height: '220'}, 2000);
+		$('.group18').animate({height: '220'}, 2000);
+		$('.group19').animate({height: '220'}, 2000);
+		$('.group20').animate({height: '220'}, 2000);
+		$('.group21').animate({height: '220'}, 2000);
+		$('.group22').animate({height: '220'}, 2000);
+
+		$('.group1').animate({height: '-=220'}, 2000);
+		$('.group2').animate({height: '-=220'}, 2000);
+		$('.group3').animate({height: '-=220'}, 2000);
+		$('.group4').animate({height: '-=220'}, 2000);
+		$('.group5').animate({height: '-=220'}, 2000);
+		$('.group6').animate({height: '-=220'}, 2000);
+		$('.group7').animate({height: '-=220'}, 2000);
+		$('.group8').animate({height: '-=220'}, 2000);
+		$('.group9').animate({height: '-=220'}, 2000);
+		$('.group10').animate({height: '-=220'}, 2000);
+		$('.group11').animate({height: '-=220'}, 2000);
+		$('.group12').animate({height: '-=220'}, 2000);
+		$('.group13').animate({height: '-=220'}, 2000);
+		$('.group14').animate({height: '-=220'}, 2000);
+		$('.group15').animate({height: '-=220'}, 2000);
+		$('.group16').animate({height: '-=220'}, 2000);
+		$('.group17').animate({height: '-=220'}, 2000);
+		$('.group18').animate({height: '-=220'}, 2000);
+		$('.group19').animate({height: '-=220'}, 2000);
+		$('.group20').animate({height: '-=220'}, 2000);
+		$('.group21').animate({height: '-=220'}, 2000);
+		$('.group22').animate({height: '-=220'}, 2000);
+	});
+
+
+// 	$('#tri46').hover('click touchstart', function() {
+// 		$(this).animate({opacity: '.9'}, 5);
+// 		var color = ["#FF5F00", "orange", "yellow", "blue", "#00F3FF", "#8BFF00", "#AA00E8", "white"];
+// var random9 = Math.round(Math.random() * 7);
+//  $( ".top" ).css("background-color", color[random9]);
+// $( ".bottom" ).css("background-color", color[random9]);
+// 		$('.middlesmall').animate({opacity: '.05'}, 2000);
+// 		$('.middlebig').animate({opacity: '.1'}, 2000);
+// 		$('.top').animate({opacity: '.1'}, 2000);
+// 		$('.bottom').animate({opacity: '.1'}, 2000);
+// 		$('.top').animate({opacity: '.22'}, 2000);
+// 		$('.bottom').animate({opacity: '.19'}, 2000);
+		
+// 	});
+
+		$('#tri16').hover('click touchstart', function() {
+			var color = ["#FF5F00", "orange", "yellow", "blue", "#00F3FF", "#8BFF00", "#AA00E8", "white"];
+var random10 = Math.round(Math.random() * 7);
+ $( ".top" ).css("background-color", color[random10]);
+$( ".bottom" ).css("background-color", color[random10]);
+		$('.top').animate({opacity: '.1'}, 3000);
+		$('.top').animate({opacity: '.2'}, 3000);
+		$('.top').animate({opacity: '.1'}, 3000);
+		$('.top').animate({opacity: '.3'}, 3000);
+		$('.top').animate({opacity: '.1'}, 3000);
+		$('.top').animate({opacity: '.4'}, 3000);
+		$('.top').animate({opacity: '.1'}, 3000);
+		$('.top').animate({opacity: '.5'}, 3000);
+		$('.top').animate({opacity: '.1'}, 3000);
+		$('.top').animate({opacity: '.9'}, 3000);
+		$('.top').animate({opacity: '.1'}, 3000);
+		$('.top').animate({opacity: '.22'}, 4000);
+		$('.bottom').animate({opacity: '.19'}, 4000);
+	
+
+	});
+
+// $('#tri2').hover('click touchstart', function() {
+// 		$('.topleft').animate({opacity: '.4'}, 2000);
+// 	});
+
+	$('#tri18').hover('click touchstart', function() {
+		var color = ["#FF5F00", "orange", "yellow", "blue", "#00F3FF", "#8BFF00", "#AA00E8", "white"];
+var random11 = Math.round(Math.random() * 7);
+ $( ".top" ).css("background-color", color[random11]);
+$( ".bottom" ).css("background-color", color[random11]);
+		$('.group1').animate({opacity: '.1'}, 1000);
+		$('.group2').animate({opacity: '.1'}, 3000);
+		$('.group3').animate({opacity: '.1'}, 1000);
+		$('.group4').animate({opacity: '.1'}, 3000);
+		$('.group5').animate({opacity: '.1'}, 1000);
+		$('.group6').animate({opacity: '.1'}, 3000);
+		$('.group7').animate({opacity: '.1'}, 1000);
+		$('.group8').animate({opacity: '.1'}, 3000);
+		$('.group9').animate({opacity: '.1'}, 1000);
+		$('.group10').animate({opacity: '.1'}, 3000);
+		$('.group11').animate({opacity: '.1'}, 1000);
+		$('.group12').animate({opacity: '.1'}, 3000);
+		$('.group13').animate({opacity: '.1'}, 1000);
+		$('.group14').animate({opacity: '.1'}, 3000);
+		$('.group15').animate({opacity: '.1'}, 1000);
+		$('.group16').animate({opacity: '.1'}, 3000);
+		$('.group17').animate({opacity: '.1'}, 1000);
+		$('.group18').animate({opacity: '.1'}, 3000);
+		$('.group19').animate({opacity: '.1'}, 1000);
+		$('.group20').animate({opacity: '.1'}, 3000);
+		$('.group21').animate({opacity: '.1'}, 1000);
+		$('.group22').animate({opacity: '.1'}, 3000);
+		$('.top').animate({opacity: '.22'}, 4000);
+		$('.bottom').animate({opacity: '.19'}, 4000);
+
+		$('.group1').animate({opacity: '.1'}, 3000);
+		$('.group2').animate({opacity: '.1'}, 1000);
+		$('.group3').animate({opacity: '.1'}, 3000);
+		$('.group4').animate({opacity: '.1'}, 1000);
+		$('.group5').animate({opacity: '.1'}, 3000);
+		$('.group6').animate({opacity: '.1'}, 1000);
+		$('.group7').animate({opacity: '.1'}, 3000);
+		$('.group8').animate({opacity: '.1'}, 1000);
+		$('.group9').animate({opacity: '.1'}, 3000);
+		$('.group10').animate({opacity: '.1'}, 1000);
+		$('.group11').animate({opacity: '.1'}, 3000);
+		$('.group12').animate({opacity: '.1'}, 1000);
+		$('.group13').animate({opacity: '.1'}, 3000);
+		$('.group14').animate({opacity: '.1'}, 1000);
+		$('.group15').animate({opacity: '.1'}, 3000);
+		$('.group16').animate({opacity: '.1'}, 1000);
+		$('.group17').animate({opacity: '.1'}, 3000);
+		$('.group18').animate({opacity: '.1'}, 1000);
+		$('.group19').animate({opacity: '.1'}, 3000);
+		$('.group20').animate({opacity: '.1'}, 1000);
+		$('.group21').animate({opacity: '.1'}, 3000);
+		$('.group22').animate({opacity: '.1'}, 1000);
+		$('.top').animate({opacity: '.22'}, 4000);
+		$('.bottom').animate({opacity: '.19'}, 4000);
+
+		$('.group1').animate({opacity: '.1'}, 1000);
+		$('.group2').animate({opacity: '.1'}, 3000);
+		$('.group3').animate({opacity: '.1'}, 1000);
+		$('.group4').animate({opacity: '.1'}, 3000);
+		$('.group5').animate({opacity: '.1'}, 1000);
+		$('.group6').animate({opacity: '.1'}, 3000);
+		$('.group7').animate({opacity: '.1'}, 1000);
+		$('.group8').animate({opacity: '.1'}, 3000);
+		$('.group9').animate({opacity: '.1'}, 1000);
+		$('.group10').animate({opacity: '.1'}, 3000);
+		$('.group11').animate({opacity: '.1'}, 1000);
+		$('.group12').animate({opacity: '.1'}, 3000);
+		$('.group13').animate({opacity: '.1'}, 1000);
+		$('.group14').animate({opacity: '.1'}, 3000);
+		$('.group15').animate({opacity: '.1'}, 1000);
+		$('.group16').animate({opacity: '.1'}, 3000);
+		$('.group17').animate({opacity: '.1'}, 1000);
+		$('.group18').animate({opacity: '.1'}, 3000);
+		$('.group19').animate({opacity: '.1'}, 1000);
+		$('.group20').animate({opacity: '.1'}, 3000);
+		$('.group21').animate({opacity: '.1'}, 1000);
+		$('.group22').animate({opacity: '.1'}, 3000);
+		$('.top').animate({opacity: '.22'}, 4000);
+		$('.bottom').animate({opacity: '.19'}, 4000);
+	
+		});
+
+
+// var rotation = 0;
+
+// jQuery.fn.rotate = function(degrees) {
+//     $(this).css({'transform' : 'rotate('+ degrees +'deg)'});
+//     return $(this);
+// };
+
+// $('#tri2').hover(function() {
+//     rotation += 180;
+//     $('.top').rotate(rotation);
+//      $('.bottom').animate({opacity: '.05'}, 1200);
+//      $('.bottom').animate({opacity: '.2'}, 1200);
+//      $('.bottom').animate({opacity: '.05'}, 1200);
+//      $('.bottom').animate({opacity: '.2'}, 1200);
+//      $('.bottom').animate({opacity: '.05'}, 1200);
+//      $('.bottom').animate({opacity: '.2'}, 1200);
+//      $('.top').rotate(rotation);
+//      $('.top').animate({opacity: '.22'}, 4000);
+// 	$('.bottom').animate({opacity: '.19'}, 4000);
+// });
+
+// $('#tri2').hover(function() {
+//     rotation += 90;
+//      $('.bottom').animate({left: '70px'}, 1200);
+//      $('.top').animate({right: '70px'}, 1200);
+//      $('.bottom').rotate(rotation);
+//      $('.top').rotate(rotation);
+//       $('.bottom').animate({right: '70px'}, 1200);
+//      $('.top').animate({left: '70px'}, 1200);
+//      $('.bottom').rotate(rotation);
+//      $('.top').rotate(rotation);
+// });
+
+
+
+// $('#tri20').hover(function() {
+//     rotation += 30;
+//     $('.bottom').animate({height: '20px', width: '20px'}, 400);
+//     $('.bottom').rotate(rotation);
+    
+// });
+
+
+// 	$('.all').hover(function(){
+//     $(this).css("background-color", "yellow");
+//     }, function(){
+//     $(this).css("background-color", "pink");
+// });
+
+
+
+
+
+
+
+
+
+
+
+});
+
+// @keyframes spin {
+//    from{
+//        transform: rotate(0deg);
+//        }to{
+//            transform: rotate(360deg);
+//         }
+//    }
+
+// [10:18]  
+// animation: spin 1s infinite linear;
+
+// });
+
+
+
+
